2c0d612594249416ee13a2ded2aeac88df3c3ad1
Ericktati1234 <91512625+Ericktati1234@users.noreply.github.com>
2024-05-02 15:44:33 -0600

    23.- Hacer un programa que imprima los números del 0 al 100, controlando las filas y las columnas.

---
+++ 23Ejercicio.js
@@ -0,0 +1,9 @@
+
+//23.- Hacer un programa que imprima los números del 0 al 100, controlando las filas y las
+//columnas. 
+;
+for (let i = 1; i <= 100; i++) {
+
+    let espacios = " ".repeat(i);
+    console.log(espacios + i);
+}
