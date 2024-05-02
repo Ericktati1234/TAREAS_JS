998656b6afdae8e75a4861b55cc0bf70f17c2bf2
Ericktati1234 <91512625+Ericktati1234@users.noreply.github.com>
2024-05-02 15:39:53 -0600

    4.- Hacer un programa que imprima la suma de los 100 primeros números.

---
+++ 4Ejercicio.js
@@ -0,0 +1,13 @@
+
+
+//4.- Hacer un programa que imprima la suma de los 100 primeros números. 
+
+var result = 0;
+function Numeros (){
+    for(let i=1;i<=100;i++){
+        result += i;
+    }
+    console.log(result);
+}
+
+Numeros()
